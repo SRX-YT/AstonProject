96dd0c57e877f9db51986f17b77b2656a84862b4
Anvar1234 <kingartaved@yandex.ru>
2024-09-25 15:24:16 +0500

    feat: добавил пакеты для стратегий random, parse, prompt.

--- src/service/strategy/input/DataInputter.java
+++ src/service/strategy/input/DataInputter.java
@@ -1,5 +1,20 @@
 package service.strategy.input;
 
-public class DataInputter<T> {
+import java.io.IOException;
+import java.util.List;
 
+public class DataInputter<T> {
+    InputStrategy<T> inputStrategy;
+
+    public DataInputter(InputStrategy<?> inputStrategy) {
+        this.inputStrategy = (InputStrategy<T>) inputStrategy;
+    }
+
+    public List<T> getInputData(int count) throws IOException {
+        return this.inputStrategy.input(count);
+    }
+
+    public InputStrategy<T> getInputStrategy() {
+        return inputStrategy;
+    }
 }
---
+++ src/service/strategy/prompt/PromptStrategy.java
@@ -0,0 +1,4 @@
+package service.strategy.prompt;
+
+public interface PromptStrategy {
+}
