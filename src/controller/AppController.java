b45cd056fe8a16a3e07739e0f2aabc1db95f556a
bezdarnosti <r403prod@gmail.com>
2024-09-27 17:05:59 +0500

    feat: AppController

--- src/controller/AppController.java
+++ src/controller/AppController.java
@@ -5,6 +5,7 @@ import service.AppService;
 import util.AppUtils;
 
 import java.io.IOException;
+import java.util.*;
 
 public class AppController {
     private final AppMenu appMenu;
@@ -17,31 +18,63 @@ public class AppController {
 
     public void run() {
 
+        Map<Integer, List<?>> products = new HashMap<>();
+
         boolean exit = false;
         while (!exit) {
-
-            int choice = appMenu.showMenu(AppMenu.MenuType.MAIN_MENU);
+            int choice = AppUtils.getValidPrompt(1, 4, appMenu, AppMenu.MenuType.MAIN_MENU);
 
             switch (choice) {
-                case 1:
+                case 1 -> {
                     try {
-                        choice = appMenu.showMenu(AppMenu.MenuType.DATA_INPUT_MENU);
-                        appService.getHandleDataInput(choice);
+                        products.clear();
+                        choice = AppUtils.getValidPrompt(1, 3, appMenu, AppMenu.MenuType.DATA_INPUT_MENU);
+                        List<?> objects = appService.getHandleDataInput(choice);
+                        if (!AppUtils.isListEmpty(objects)) { //TODO
+                            products.put(0, objects);
+                        }
                     } catch (IOException e) {
-                        throw new RuntimeException(e);
+                        System.out.println("\nОшибка ввода данных: " + e.getMessage());
+                        System.out.println("Пожалуйста, повторите ввод.");
                     }
-                    break;
-                case 2:
-//                   AppService.getHandleSorting();
-                    break;
-                case 3:
-//                    AppService.getHandleBinarySearch();
-                    break;
-                case 4:
-                    exit = true;
-                    break;
-                default:
-                    System.out.println("Некорректный выбор. Повторите.");
+                }
+                case 2 -> {
+                    if (!products.containsKey(0)) {
+                        System.out.println("\nСначала следует заполнить список.");
+                    } else {
+                        choice = AppUtils.getValidPrompt(1, 2, appMenu, AppMenu.MenuType.SORTING_MENU);
+                        switch (choice) {
+                            case 1 -> {
+                                products.put(1, appService.getHandleSorting(products.get(0), choice));
+                                products.get(1).forEach(System.out::println);
+                            }
+                            case 2 -> {
+                                if (!products.containsKey(1)) {
+                                    products.put(2, appService.getHandleSorting(products.get(0), choice));
+                                    products.get(2).forEach(System.out::println);
+                                } else {
+                                    List<?> temp = products.get(0);
+                                    products.put(2, appService.getHandleSorting(temp, choice));
+                                    temp.forEach(System.out::println);
+                                }
+                            }
+                            default -> System.out.println("\nНекорректный выбор. Повторите.");
+                        }
+                    }
+                }
+
+                case 3 -> {
+                    if (!products.containsKey(0)) {
+                        System.out.println("\nСначала следует заполнить список.");
+                    } else if (!products.containsKey(1)) {
+                        System.out.println("\nСначала следует отсортировать список через MergeSort.");
+                    } else {
+                        appService.getHandleBinarySearch(products.get(1));
+                    }
+                }
+
+                case 4 -> exit = true;
+                default -> System.out.println("Некорректный выбор. Повторите.");
             }
         }
     }
